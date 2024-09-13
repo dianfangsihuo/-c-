8b25ef9d214d7e1a0b9d856f25dcbac985020af1
dianfangsihuo <dianfangxuezhixia@qq.com>
2024-09-13 23:55:59 +0800

    Add files via upload

---
+++ 水仙.cpp
@@ -0,0 +1,13 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b, c, d;
+//	scanf("%d", &a);
+//	b = a / 100;
+//	c = a / 10 % 10;
+//	d = a % 10;
+//	if (a == b * b*b + c * c*c + d * d*d)
+//		printf("yes");
+//	else printf("no");
+//	return 0;
+//}
