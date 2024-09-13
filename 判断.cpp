c7c955fd2f377b7858d792686debbf2086344821
dianfangsihuo <dianfangxuezhixia@qq.com>
2024-09-13 23:08:35 +0800

    Add files via upload

---
+++ 判断.cpp
@@ -0,0 +1,21 @@
+
+//#include <stdio.h>
+//int main()
+//{
+//	char a;
+//	scanf("%c", &a);
+//	if (a >= 65 && a <= 90)
+//	{
+//		printf("upper");
+//	}
+//	else if (a >= 97 && a <= 122)
+//	{
+//		printf("lower");
+//	}
+//	else if (a >= 48 && a <= 57)
+//	{
+//		printf("digit");
+//	}
+//	else printf("other");
+//	return 0;
+//}
