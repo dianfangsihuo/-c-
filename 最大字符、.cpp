98b701f1d34e01902e22fb6c5055a899ce1010e2
dianfangsihuo <dianfangxuezhixia@qq.com>
2024-09-14 23:04:26 +0800

    Add files via upload

---
+++ 最大字符、.cpp
@@ -0,0 +1,19 @@
+//#include <stdio.h>
+//int main()
+//{
+//	char a, b, c;
+//	scanf("%c %c %c", &a, &b, &c);
+//	if (a>b&&a>c)
+//	{
+//		printf("%c\n", a);
+//	}
+//	if (b>a&&b>c)
+//	{
+//		printf("%c\n", b);
+//	}
+//	if (c > a&&c > b)
+//	{
+//		printf("%c\n", c);
+//	}
+//	return 0;
+//}
