4df7d8e938180f74e31a4b786018b04f431fc2ee
dianfangsihuo <dianfangxuezhixia@qq.com>
2024-09-14 23:05:04 +0800

    Add files via upload

---
+++ 判断直角三角形.cpp
@@ -0,0 +1,11 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b, c;
+//	scanf("%d%d%d", &a, &b, &c);
+//	if (a * a + b * b == c * c || a * a + c * c == b * b || c * c + b * b == a * a&&a + b > c && a + c > b && b + c > a)
+//		printf("Yes");
+//	else
+//		printf("No");
+//	return 0;
+//}
