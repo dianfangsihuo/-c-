45ed6824d0635eeea4472d504029269129f4a2d7
dianfangsihuo <dianfangxuezhixia@qq.com>
2024-09-15 23:01:50 +0800

    Add files via upload

---
+++ 薪水.cpp
@@ -0,0 +1,14 @@
+//#include<stdio.h>
+//int main()
+//{
+//	double yuan,t;
+//	scanf("%lf", &yuan);
+//	if (yuan <= 10000)
+//		t = yuan * 0.05;
+//	if (yuan > 10000 && yuan <= 50000)
+//		t = 500 + (yuan - 10000) * 0.03;
+//	if (yuan > 50000)
+//		t = 500 + 1200 + (yuan - 50000) * 0.02;
+//	printf("%.2f", 1500 + t);
+//	return 0;
+//}
