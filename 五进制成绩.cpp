2fed5e4e0276e62eca08993a9b983791fc67d0a4
dianfangsihuo <dianfangxuezhixia@qq.com>
2024-09-16 22:22:22 +0800

    Add files via upload

---
+++ 五进制成绩.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	scanf("%d", &a);
+//	if (a >= 90)
+//		printf("A");
+//	if (a >= 80 && a < 90)
+//		printf("B");
+//	if (a >= 70 && a < 80)
+//		printf("C");
+//	if (a >= 60 && a < 70)
+//		printf("D");
+//	if (a < 60)
+//		printf("E");
+//	
+//	return 0;
+//}
