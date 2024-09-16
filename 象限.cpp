158eaa4401a37218ac5b34d845ea581512357dcd
dianfangsihuo <dianfangxuezhixia@qq.com>
2024-09-16 22:22:36 +0800

    Add files via upload

---
+++ 象限.cpp
@@ -0,0 +1,15 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b;
+//	scanf("%d%d", &a, &b);
+//	if (a > 0&& b > 0)
+//		printf("1");
+//	if (a > 0 && b < 0)
+//		printf("4");
+//	if (a < 0 && b < 0)
+//		printf("3");
+//	if (a < 0 && b>0)
+//		printf("2");
+//	return 0;
+//}
